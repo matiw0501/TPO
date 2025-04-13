c8ae004c677b00f3c92202f87242e51fbfbfa120
Mateusz W <s31224@pjwstk.edu.pl>
2025-04-13 14:20:19 +0200

    just reading and errors logic. To do all logic of counting etc

--- TPO3_WM_S31224/src/zad1/Time.java
+++ TPO3_WM_S31224/src/zad1/Time.java
@@ -7,5 +7,39 @@
 package zad1;
 
 
+
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+
+
 public class Time {
+    public static String passed(String from, String to) {
+        LocalDateTime localDateTime = null; // LocalDateTime.parse("2000-02-02T10:15:30");
+        LocalDate localDate = null;
+        try {
+           //  a = LocalDateTime.parse(to, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+            localDate = LocalDate.parse(to, DateTimeFormatter.ISO_LOCAL_DATE);
+            return localDate.toString();
+
+        }
+        catch (DateTimeParseException e) {
+           try{
+               localDateTime = LocalDateTime.parse(to, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+               return localDateTime.toString();
+           }
+           catch (DateTimeException ex) {
+               String [] arr = to.split("-").clone();
+               if (!(Integer.parseInt(arr[0]) % 4 == 0) && Integer.parseInt(arr[1]) == 2 && Integer.parseInt(arr[2]) == 29 ) {
+                  return "***" + e;
+               }
+               return("***" + ex);
+           }
+        }
+
+
+
+    }
 }
--- TPO3_WM_S31224/src/zad1/Tools.java
+++ TPO3_WM_S31224/src/zad1/Tools.java
@@ -7,5 +7,19 @@
 package zad1;
 
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import java.util.stream.Collectors;
+
 public class Tools {
+    static Options createOptionsFromYaml(String fileName) throws Exception {
+            String yaml = Files.readAllLines(Paths.get(fileName)).stream().collect(Collectors.joining(System.lineSeparator()));
+            Map<String, Object> map = new Yaml().loadAs(yaml, Map.class);
+        return new Options((String) map.get("host"), (int)map.get("port"), (boolean)map.get("concurMode"), (boolean) map.get("showSendRes"), (Map<String, List<String>>)map.get("clientsMap"));
+    }
 }
